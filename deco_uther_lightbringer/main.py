5a48b90bb30ebea1f5776606c13ee69b28a9cfe0
DmitriyBurtsev <dm.burcev@yandex.ru>
2023-04-20 23:04:10 +0300

    Done Block 1 Easy Task 1

--- deco_uther_lightbringer/main.py
+++ deco_uther_lightbringer/main.py
@@ -2,9 +2,19 @@
 # Easy
 # Задача 1
 
+def multiply_string_by_number(line: str, number: int) -> str:
+    current_line = ''
+    for i in range(number):
+        if i % 2 == 1:
+            current_line += line.upper()
+        else:
+            current_line += line
+    return current_line
+
+print(multiply_string_by_number('test', 3))
 
 
-
+# Задача 2
 
 
 
